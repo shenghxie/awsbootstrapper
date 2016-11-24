5b00570f06462107b48981066fedb975ea65c3ef
Scott Morken <smorken@nrcan.gc.ca>
2016-11-23 16:30:08 -0800

    add uploader

--- uploader.py
+++ uploader.py
@@ -4,10 +4,10 @@ from loghelper import LogHelper
 
 def main():
 
-    LogHelper.start_logging("downloader.log")
+    LogHelper.start_logging("uploader.log")
     parser = argparse.ArgumentParser(
-        description="AWS bootstrapper downloader" +
-                    "Downloads AWSToLocal jobs as specified in manifest from S3")
+        description="AWS bootstrapper uploader" +
+                    "Uploades manifest and LocalToAWS documents described in manifest to S3")
 
     parser.add_argument("--manifestPath", help = "path to a manifest file describing the jobs and data requirements for the application", required=True)
     parser.add_argument("--localWorkingDir", help = "path to dir with read/write for processing data for upload and download to AWS S3", required=True)
@@ -19,10 +19,9 @@ def main():
         s3 = boto3.resource('s3')
 
         app = Application(s3, manifestPath, localWorkingDir)
-        app.downloadS3Documents()
-
+        app.uploadS3Documents()
     except Exception as ex:
-        logging.exception("error in downloader")
+        logging.exception("error in launcher")
         sys.exit(-1)
 
 if __name__ == "__main__":
