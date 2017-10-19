4440717a516b87f82a82f0803612704766c6609c
Scott Morken <smorken@nrcan.gc.ca>
2017-10-19 15:12:40 -0700

    was using built in function id (and was passing test cases somehow)

--- instancemetadatafactory.py
+++ instancemetadatafactory.py
@@ -9,7 +9,7 @@ class InstanceMetadataFactory(object):
 
     def InitializeMetadata(self, id, aws_id):
         ncommands = len(self.manifest.GetJob(id)["Commands"])
-        nuploads = self._GetUploadCount()
+        nuploads = self._GetUploadCount(id)
         ndownloads = len(self.manifest.GetJob(id)["RequiredS3Data"]) - nuploads
         inst = InstanceMetadata(id=id,
                                 aws_id=aws_id,
@@ -23,7 +23,7 @@ class InstanceMetadataFactory(object):
                                 ndownloadsFinished = 0)
         return inst
 
-    def _GetUploadCount(self):
+    def _GetUploadCount(self, id):
         count = 0
         alldocs = { x["Name"] : x for x in self.manifest.GetS3Documents() }
         for r in self.manifest.GetJob(id)["RequiredS3Data"]:
